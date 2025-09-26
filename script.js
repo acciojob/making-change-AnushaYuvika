2779db45d10828a676c2c4ea86a45195c481275c
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-09-27 02:34:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const makeChange = (c) => {
   // your name here
+	let rem = c;
+
+  // calculate number of coins
+  const q = Math.floor(rem / 25);
+  remaining %= 25;
+
+  const d = Math.floor(rem / 10);
+  remaining %= 10;
+
+  const n = Math.floor(rem / 5);
+  remaining %= 5;
+
+  const p = rem; // pennies left
+
+  return { q, d, n, p };
 };
 
 // Do not the change the code below
