26e73b70250974ac5a0bc7addc840aa8601bdf78
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-09-27 02:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const makeChange = (c) => {
   // your name here
 	let rem = c;
 
-  // calculate number of coins
   const q = Math.floor(rem / 25);
-  remaining %= 25;
+  rem %= 25;
 
   const d = Math.floor(rem / 10);
-  remaining %= 10;
+  rem %= 10;
 
   const n = Math.floor(rem / 5);
-  remaining %= 5;
+  rem %= 5;
 
   const p = rem; // pennies left
 
@@ -18,5 +17,5 @@ const makeChange = (c) => {
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
